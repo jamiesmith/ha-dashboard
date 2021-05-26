21fb5b583d0cfb0228b38ddb9bd0abd1f48cfae8
Jamie Smith <github@jamieandamy.com>
2021-05-26 16:25:47 -0400

    add mqtt publish component

--- custom_widgets/basemedia/basemedia.js
+++ custom_widgets/basemedia/basemedia.js
@@ -1,6 +1,6 @@
-function basecombosonos(widget_id, url, skin, parameters)
+function basemedia(widget_id, url, skin, parameters)
 {
-	self = this;
+    self = this;
 
     // Initialization
 
@@ -15,7 +15,6 @@ function basecombosonos(widget_id, url, skin, parameters)
     self.OnNextButtonClick = OnNextButtonClick;
     self.OnRaiseLevelClick = OnRaiseLevelClick;
     self.OnLowerLevelClick = OnLowerLevelClick;
-    self.OnPlaylistSelectChange = OnPlaylistSelectChange;
 
     self.min_level = 0;
     self.max_level = 1;
@@ -26,17 +25,16 @@ function basecombosonos(widget_id, url, skin, parameters)
     }
     else
     {
-        self.step = 0.02;
+        self.step = 0.1;
     }
 
     var callbacks =
         [
-            {"selector": '#' + widget_id + ' #play',            "action": "click",  "callback": self.OnPlayButtonClick},
-            {"selector": '#' + widget_id + ' #level-up',        "action": "click",  "callback": self.OnRaiseLevelClick},
-            {"selector": '#' + widget_id + ' #level-down',      "action": "click",  "callback": self.OnLowerLevelClick},
-            {"selector": '#' + widget_id + ' #previous',        "action": "click",  "callback": self.OnPreviousButtonClick},
-            {"selector": '#' + widget_id + ' #next',            "action": "click",  "callback": self.OnNextButtonClick},
-	    {"selector": '#' + widget_id + ' #select-playlist', "action": "change", "callback": self.OnPlaylistSelectChange}
+            {"selector": '#' + widget_id + ' #play', "action": "click", "callback": self.OnPlayButtonClick},
+            {"selector": '#' + widget_id + ' #level-up', "action": "click", "callback": self.OnRaiseLevelClick},
+            {"selector": '#' + widget_id + ' #level-down', "action": "click", "callback": self.OnLowerLevelClick},
+            {"selector": '#' + widget_id + ' #previous', "action": "click", "callback": self.OnPreviousButtonClick},
+            {"selector": '#' + widget_id + ' #next', "action": "click", "callback": self.OnNextButtonClick}
         ];
 
     // Define callbacks for entities - this model allows a widget to monitor multiple entities if needed
@@ -49,11 +47,9 @@ function basecombosonos(widget_id, url, skin, parameters)
     var monitored_entities =
         [
             {"entity": parameters.entity, "initial": self.OnStateAvailable, "update": self.OnStateUpdate}
-			
         ];
 
     // Finally, call the parent constructor to get things moving
-	//
 
     WidgetBase.call(self, widget_id, url, skin, parameters, monitored_entities, callbacks);
 
@@ -65,23 +61,13 @@ function basecombosonos(widget_id, url, skin, parameters)
 
     function OnStateAvailable(self, state)
     {
-	console.log("OnStateAvailable", self, state);
-	
-	self.entity = state.entity_id;
+        self.entity = state.entity_id;
         self.level = state.attributes.volume_level;
-	self.state = state.state;
-	self.source = state.source;
-	
-	
         set_view(self, state)
         if ("dump_capabilities" in self.parameters && self.parameters["dump_capabilities"] == "1")
         {
-	    console.log("OnStateAvailable self => ", self);
-            display_supported_functions(self);
+            display_supported_functions(self)
         }
-	
-	set_playlist_options(self, state.attributes.source_list, state);
-	set_playlist_value(self, state);
     }
 
     // The OnStateUpdate function will be called when the specific entity
@@ -90,8 +76,8 @@ function basecombosonos(widget_id, url, skin, parameters)
 
     function OnStateUpdate(self, state)
     {
-	self.level = state.attributes.volume_level;
-        console.log("state update" , state);
+	console.log("OnStateUpdate", self, state);
+        self.level = state.attributes.volume_level;
         set_view(self, state)
     }
 
@@ -101,16 +87,8 @@ function basecombosonos(widget_id, url, skin, parameters)
         {
             if (is_supported(self, "PLAY_MEDIA"))
             {
-				// Don't want to just "play", but rather play the selected playlist
-				//
-                // args = self.parameters.post_service_play_pause;
-                // self.call_service(self, args)
-				
-				args = self.parameters.post_service_select_playlist
-				
-				args["source"] = self.ViewModel.selectedoption();
-				
-				self.call_service(self, args);				
+                args = self.parameters.post_service_play_pause;
+                self.call_service(self, args)
             }
             else
             {
@@ -122,6 +100,11 @@ function basecombosonos(widget_id, url, skin, parameters)
             if (is_supported(self, "PAUSE"))
             {
                 args = self.parameters.post_service_pause;
+                self.call_service(self, args)
+            }
+            else if (is_supported(self, "STOP"))
+            {
+                args = self.parameters.post_service_stop;
                 self.call_service(self, args)
             }
             else if (is_supported(self, "STOP"))
@@ -163,51 +146,8 @@ function basecombosonos(widget_id, url, skin, parameters)
             console.log("NEXT_TRACK attribute not supported")
         }
     }
-	
-	function OnPlaylistSelectChange(self, state)
-	{
-		var curEntity = self.entity_state[self.entity];
 
-		var entityState = curEntity.state;
-		var entitySource = curEntity.attributes.source;
-		var entityName = curEntity.entity_id;
-		var selectedSource = self.ViewModel.selectedoption();
-		var selectedState = self.state;
-		
-		self.source = selectedSource;
 
-		setTimeout(function(){
-			// console.log(self.entity_state[self.entity].entity_id + " fcn entityState => ", entityState);
-			// console.log(self.entity_state[self.entity].entity_id + " fcn entitySource => ", entitySource);
-			// console.log(self.entity_state[self.entity].entity_id + " fcn entityName => ", entityName);
-			// console.log(self.entity_state[self.entity].entity_id + " fcn selectedSource => ", selectedSource);
-			// console.log(self.entity_state[self.entity].entity_id + " fcn selectedState => ", selectedState);
-
-			// What I do have to figure out is, what happens when something OTHER than a "source" is playing.  
-			// Right now a page reload wipes it out and starts playing the first thing in the list
-			// adding !curEntity.attributes.media_album_name is close, but then the playlist change isn't detected
-			//
-			if ((self.state === "playing") 
-				&& (entitySource != self.ViewModel.selectedoption())
-				&& (!curEntity.attributes.media_album_name))
-			{	
-				// console.log("######################## They Dont Match!");
-		
-				
-				self.source = self.ViewModel.selectedoption();
-				args = self.parameters.post_service_select_playlist
-				
-				args["source"] = self.source
-				// console.log("Calling Service => ", args);
-
-				self.call_service(self, args);
-			}
-			else
-			{
-				// console.log("######################## They Match!");
-			}
-		},500)
-	}	
 
     function OnRaiseLevelClick(self)
     {
@@ -234,33 +174,21 @@ function basecombosonos(widget_id, url, skin, parameters)
         args = self.parameters.post_service_level;
         args["volume_level"] = self.level;
         self.call_service(self, args)
+
+
     }
-
-	function set_playlist_options(self, options, state)
-	{
-		// console.log("JRS", options)
-		self.set_field(self, "inputoptions", options);
-	}
-
-	function set_playlist_value(self, state)
-	{
-		value = state.attributes.source
-		self.set_field(self, "selectedoption", value)
-	}
 
     function set_view(self, state)
     {
         if (state.state === "playing")
         {
-            self.set_field(self, "state", "playing");
-            self.set_field(self, "play_icon_style", self.css.icon_style_active);
+            self.set_field(self, "play_icon_style", self.css.icon_style_active)
             self.set_icon(self, "play_icon", self.icons.pause_icon)
         }
         else
         {
-            self.set_field(self, "state", self.entity_state[self.entity].state);
-            self.set_field(self, "play_icon_style", self.css.icon_style_inactive);
-            self.set_icon(self, "play_icon", self.icons.play_icon);
+            self.set_field(self, "play_icon_style", self.css.icon_style_inactive)
+            self.set_icon(self, "play_icon", self.icons.play_icon)
         }
 
         if ("media_artist" in state.attributes)
@@ -272,7 +200,6 @@ function basecombosonos(widget_id, url, skin, parameters)
         {
             self.set_field(self, "album", state.attributes.media_album_name)
         }
-		
         if ("media_title" in state.attributes)
         {
             if ("truncate_name" in self.parameters)
@@ -281,23 +208,17 @@ function basecombosonos(widget_id, url, skin, parameters)
             }
             else
             {
-                name = state.attributes.media_title;
+                name = state.attributes.media_title
             }
             self.set_field(self, "media_title", name);
         }
-		
-		if ("source" in state.attributes)
-		{
-			self.set_field(self, "selectedoption", state.attributes.source)
-		}
-		
         if ("volume_level" in state.attributes)
         {
-            self.set_field(self, "level", Math.round(state.attributes.volume_level * 100));
+            self.set_field(self, "level", Math.round(state.attributes.volume_level * 100))
         }
         else
         {
-            self.set_field(self, "level", 0);
+            self.set_field(self, "level", 0)
         }
 
     }
@@ -330,17 +251,17 @@ function basecombosonos(widget_id, url, skin, parameters)
             var attr_value = support[attr];
             if ((supported & attr_value) == attr_value)
             {
-                return true;
+                return true
             }
             else
             {
-                return false;
+                return false
             }
         }
         else
         {
-            console.log("Unknown media player attribute: " + attr);
-            return false;
+            console.log("Unknown media player attribute: " + attr)
+            return false
         }
     }
 
@@ -348,20 +269,20 @@ function basecombosonos(widget_id, url, skin, parameters)
     {
         console.log(self.parameters.entity);
         console.log("Supported Features: " + self.entity_state[parameters.entity].attributes.supported_features);
-        console.log("PAUSE: " + is_supported(self, "PAUSE"));
-        console.log("SEEK: " + is_supported(self, "SEEK"));
-        console.log("VOLUME_SET: " + is_supported(self, "VOLUME_SET"));
-        console.log("VOLUME_MUTE: " + is_supported(self, "VOLUME_MUTE"));
-        console.log("PREVIOUS_TRACK: " + is_supported(self, "PREVIOUS_TRACK"));
-        console.log("NEXT_TRACK: " + is_supported(self, "NEXT_TRACK"));
-        console.log("TURN_ON: " + is_supported(self, "TURN_ON"));
-        console.log("TURN_OFF: " + is_supported(self, "TURN_OFF"));
-        console.log("PLAY_MEDIA: " + is_supported(self, "PLAY_MEDIA"));
-        console.log("VOLUME_STEP: " + is_supported(self, "VOLUME_STEP"));
-        console.log("SELECT_SOURCE: " + is_supported(self, "SELECT_SOURCE"));
-        console.log("STOP: " + is_supported(self, "STOP"));
-        console.log("CLEAR_PLAYLIST: " + is_supported(self, "CLEAR_PLAYLIST"));
-        console.log("PLAY: " + is_supported(self, "PLAY"));
-        console.log("SHUFFLE_SET: " + is_supported(self, "SHUFFLE_SET"));
+        console.log("PAUSE: " + is_supported(self, "PAUSE"))
+        console.log("SEEK: " + is_supported(self, "SEEK"))
+        console.log("VOLUME_SET: " + is_supported(self, "VOLUME_SET"))
+        console.log("VOLUME_MUTE: " + is_supported(self, "VOLUME_MUTE"))
+        console.log("PREVIOUS_TRACK: " + is_supported(self, "PREVIOUS_TRACK"))
+        console.log("NEXT_TRACK: " + is_supported(self, "NEXT_TRACK"))
+        console.log("TURN_ON: " + is_supported(self, "TURN_ON"))
+        console.log("TURN_OFF: " + is_supported(self, "TURN_OFF"))
+        console.log("PLAY_MEDIA: " + is_supported(self, "PLAY_MEDIA"))
+        console.log("VOLUME_STEP: " + is_supported(self, "VOLUME_STEP"))
+        console.log("SELECT_SOURCE: " + is_supported(self, "SELECT_SOURCE"))
+        console.log("STOP: " + is_supported(self, "STOP"))
+        console.log("CLEAR_PLAYLIST: " + is_supported(self, "CLEAR_PLAYLIST"))
+        console.log("PLAY: " + is_supported(self, "PLAY"))
+        console.log("SHUFFLE_SET: " + is_supported(self, "SHUFFLE_SET"))
     }
 }
