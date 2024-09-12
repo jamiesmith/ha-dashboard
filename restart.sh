ce98001ae8786890ecba4c1645e0d602ff62d0ec
Jamie Smith <github@jamieandamy.com>
2024-09-12 10:00:45 -0400

    for some reason it was trying to create a volume

--- restart.sh
+++ restart.sh
@@ -2,13 +2,13 @@
 
 container="ha-dashboard"
 
-if [ $(docker-compose ps ${container} | grep -c Up) -gt 0 ]
+if [ $(docker compose ps ${container} | grep -c Up) -gt 0 ]
 then
-    echo "Up!"
-    docker-compose restart
+    echo "It's Up, restarting!"
+    docker compose restart
 else
-    echo "Down"
-    docker-compose up -d
+    echo "It's Down, starting"
+    docker compose up -d
 fi
 
 docker logs --tail 30 --follow ${container}
